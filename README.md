2f6f388a97a64532d92415a9f8a6fec848543108
nickchecan <nicholas.checan@icloud.com>
2025-02-14 21:25:22 -0600

    chore: implements conditional branching with async execution

--- README.md
+++ README.md
@@ -46,6 +46,31 @@ graph TD;
 	classDef last fill:#bfb6fc
 ```
 
+Conditional Branching with async execution:
+
+```mermaid
+%%{init: {'flowchart': {'curve': 'linear'}}}%%
+graph TD;
+	__start__([<p>__start__</p>]):::first
+	a(a)
+	b(b)
+	c(c)
+	d(d)
+	e(e)
+	__end__([<p>__end__</p>]):::last
+	__start__ --> a;
+	b --> e;
+	c --> e;
+	d --> e;
+	e --> __end__;
+	a -.-> b;
+	a -.-> c;
+	a -.-> d;
+	classDef default fill:#f2f0ff,line-height:1.2
+	classDef first fill-opacity:0
+	classDef last fill:#bfb6fc
+```
+
 ### Utilities
 
 Poetry command to install project dependencies:
@@ -59,3 +84,13 @@ LANGCHAIN_API_KEY=<LangSmith API key>
 LANGCHAIN_TRACING=true
 LANGCHAIN_PROJECT=AsyncProject
 ```
+
+### Drawbacks
+
+- State Conflicts: The nodes in the graph may share and change the same state, which can lead to conflicts.
+- Debugging asynchronous code can be difficult.
+
+The best practice is to isolate state updates. Each node should write to a different attribute of the state object to conserve data integrity and prevents overwriting values.
+### References
+
+[How to create branches for parallel node execution](https://langchain-ai.github.io/langgraph/how-tos/branching/)
--- async.py
+++ async.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import operator
-from typing import Annotated, Any
+from typing import Annotated, Any, Sequence
 
 from typing_extensions import TypedDict
 
@@ -10,6 +10,7 @@ from langgraph.graph import StateGraph, START, END
 
 class State(TypedDict):
     aggregate: Annotated[list, operator.add]
+    which: str
 
 class ReturnNodeValue:
 
@@ -22,25 +23,54 @@ class ReturnNodeValue:
 
 builder = StateGraph(State)
 
+# > Example 1
+# builder.add_node("a", ReturnNodeValue("I'm A"))
+# builder.add_edge(START, "a")
+# builder.add_node("b", ReturnNodeValue("I'm B"))
+# builder.add_node("b2", ReturnNodeValue("I'm B2"))
+# builder.add_node("c", ReturnNodeValue("I'm C"))
+# builder.add_node("d", ReturnNodeValue("I'm D"))
+#
+# # Creates the parallel execution automatically
+# builder.add_edge("a", "b")
+# builder.add_edge("a", "c")
+#
+# # Simpler flow
+# # builder.add_edge("b", "d")
+# # builder.add_edge("c", "d")
+# # Advanced Flow
+# builder.add_edge("b", "b2")
+# builder.add_edge(["b2", "c"], "d")
+#
+# builder.add_edge("d", END)
+# < Example 1
+
+# > Example 2
 builder.add_node("a", ReturnNodeValue("I'm A"))
 builder.add_edge(START, "a")
 builder.add_node("b", ReturnNodeValue("I'm B"))
-builder.add_node("b2", ReturnNodeValue("I'm B2"))
 builder.add_node("c", ReturnNodeValue("I'm C"))
 builder.add_node("d", ReturnNodeValue("I'm D"))
+builder.add_node("e", ReturnNodeValue("I'm E"))
 
-# Creates the parallel execution automatically
-builder.add_edge("a", "b")
-builder.add_edge("a", "c")
+def route_bc_or_cd(state: State) -> Sequence[str]:
+    if state["which"] == "cd":
+        return ["c", "d"]
+    return ["b", "c"]
 
-# Simpler flow
-# builder.add_edge("b", "d")
-# builder.add_edge("c", "d")
-# Advanced Flow
-builder.add_edge("b", "b2")
-builder.add_edge(["b2", "c"], "d")
+intermediates = ["b", "c", "d"]
 
-builder.add_edge("d", END)
+builder.add_conditional_edges(
+    "a",
+    route_bc_or_cd,
+    intermediates
+)
+
+for node in intermediates:
+    builder.add_edge(node, "e")
+
+builder.add_edge("e", END)
+# > Example 2
 
 graph = builder.compile()
 
@@ -49,4 +79,9 @@ print(graph.get_graph().draw_mermaid())
 
 if __name__ == "__main__":
     print("Hello Async Graph")
-    graph.invoke({"aggregate": []}, {"configurable": {"thread_id": "foo"}})
+    # graph.invoke({"aggregate": []}, {"configurable": {"thread_id": "foo"}}) # Example 1
+
+    # > Example 2
+    # graph.invoke({"aggregate": [], "which": ""}, {"configurable": {"thread_id": "foo"}}) # Run everything in parallel
+    graph.invoke({"aggregate": [], "which": "cd"}, {"configurable": {"thread_id": "foo"}}) # Run C and D
+    # < Example 2
